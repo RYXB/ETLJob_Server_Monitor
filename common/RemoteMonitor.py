1567d808767592719b00570857d4cfaf1e6b36b3
RYXB <570959422@qq.com>
2022-08-17 16:48:31 +0800

    优化目录树结构

--- common/RemoteMonitor.py
+++ common/RemoteMonitor.py
@@ -6,13 +6,8 @@ from LoggerTools import log
 import pandas as pd
 from DingDingMsg import DD2MSG
 
-hostname = ParamUtil.SERVER_IP
-port = ParamUtil.SERVER_PORT
-username = ParamUtil.SERVER_USER
-password = ParamUtil.SERVER_PASSWORD
 
-
-def ssh_exec(command):
+def ssh_exec(hostname,port,username,password,command):
     try:
         ssh = paramiko.SSHClient()  # 创建SSH对象
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 允许连接不在know_hosts文件中的主机
@@ -37,8 +32,8 @@ def ssh_exec(command):
 """
 内存监控
 """
-def mem_info():
-    result = ssh_exec('cat /proc/meminfo')
+def mem_info(hostname,port,username,password):
+    result = ssh_exec(hostname,port,username,password,'cat /proc/meminfo')
     mem_values = re.findall("(\d+)\ kB", str(result))
     MemTotal = mem_values[0]
     MemFree = mem_values[1]
@@ -54,15 +49,16 @@ def mem_info():
     Free_Mem = int(MemFree) + int(Buffers) + int(Cached)
     Used_Mem = int(MemTotal) - Free_Mem
     Rate_Mem = 100 * Used_Mem / float(MemTotal)
-    DD2MSG("内存状态检测:"+"\n"+"可用内存:"+ str("%.2f" % (int(MemFree)/1024/1024)) +"GB" +"\n"+"内存利用率："+str("%.2f" % Rate_Mem)+"%")
+    # DD2MSG("内存状态检测:"+"\n"+"可用内存:"+ str("%.2f" % (int(MemFree)/1024/1024)) +"GB" +"\n"+"内存利用率："+str("%.2f" % Rate_Mem)+"%")
+    return (MemFree,Rate_Mem)
 
 
 """ 
 磁盘空间监控
 """
-def disk_stat():
+def disk_info(hostname,port,username,password):
     # 将查询结果转为pandas.DF
-    result = str(ssh_exec('df -a'))
+    result = str(ssh_exec(hostname,port,username,password,'df -a'))
     data = result.strip().split('\\n')
     data.pop(0)
 
@@ -79,8 +75,56 @@ def disk_stat():
     total_sum = rs["1K-blocks"].astype("int").sum()
     available=rs["Available"].astype("int").sum()
     disk_rate = (used_sum/total_sum) * 100
-    DD2MSG("磁盘状态检测:"+"\n"+"可用磁盘:"+ str("%.2f" % (available/1024/1024)) +"GB" +"\n"+"磁盘使用率:"+str("%.2f" % disk_rate) + "%")
+    # DD2MSG("磁盘状态检测:"+"\n"+"可用磁盘:"+ str("%.2f" % (available/1024/1024)) +"GB" +"\n"+"磁盘使用率:"+str("%.2f" % disk_rate) + "%")
+    return (available,disk_rate)
 
+# 整合mem_info和disk_info方法，并输出至钉钉
+def mode_log_info(hostname, port, username, password):
+    mem = mem_info(hostname, port, username, password)
+    disk = disk_info(hostname, port, username, password)
+
+    MemFree = mem[0]
+    Rate_Mem = mem[1]
+
+    available = disk[0]
+    disk_rate = disk[1]
+
+    DD2MSG("服务器"+hostname+" 内存状态检测:"+"\n"+"可用内存:"+ str("%.2f" % (int(MemFree)/1024/1024)) +"GB" +"\t"+"内存利用率："+str("%.2f" % Rate_Mem)+"%" +"\n"
+           +"磁盘状态检测:"+"\n"+"可用磁盘:"+ str("%.2f" % (available/1024/1024)) +"GB" +"\t"+"磁盘使用率:"+str("%.2f" % disk_rate) + "%")
+
+# 将任务配置中多台服务器进行分割检测
 def check_server_status():
-    mem_info()
-    disk_stat()
+
+    hostname = ParamUtil.SERVER_IP.split(",")
+    port = ParamUtil.SERVER_PORT.split(",")
+    username = ParamUtil.SERVER_USER.split(",")
+    password = ParamUtil.SERVER_PASSWORD.split(",")
+
+    hostnameStr = ""
+    portInt = ""
+    usernameStr = ""
+    passwordStr = ""
+
+    # joblist拼接成jobstr  ['ETL_DW00', 'ETL_DW05'] -> 'ETL_DW00','ETL_DW05',
+    if hostname == [''] or port == [''] or username == [''] or password == ['']:
+        log.error("服务器状态检查任务错误:参数配置异常")
+    else:
+        log.info("服务器状态检查任务开始,当前服务器为:" + ParamUtil.SERVER_IP)
+        for item in hostname:
+            hostnameStr = hostnameStr + item + ","
+        for item in port:
+            portInt = portInt + item + ","
+        for item in username:
+            usernameStr = usernameStr + item + ","
+        for item in password:
+            passwordStr = passwordStr + item + ","
+
+    # 切片处理去掉最后一个,号
+    hostnameStr = hostnameStr[:len(hostnameStr) - 1].split(',')
+    portInt = portInt[:len(portInt) - 1].split(',')
+    usernameStr = usernameStr[:len(usernameStr) - 1].split(',')
+    passwordStr = passwordStr[:len(passwordStr) - 1].split(',')
+
+    # 调用 mode_log_info(hostname, port, username, password)检测服务器状态
+    for (hostname, port, username, password) in zip(hostnameStr, portInt, usernameStr, passwordStr):
+        mode_log_info(hostname, port, username, password)
